82e65f5a61d727718175d3a197bf2eb794bd40f2
Eloy Boone <eloyboone@hotmail.com>
2021-01-20 22:17:25 +0100

    git deployment testing

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className="bg-gray-300 border-gray-800 rounded-lg border-2 flex items-center justify-center p-4">
-      <h1>Next JS + Tailwind Template</h1>
+      <h1>Next JS + Tailwind Template (GIT TEST)</h1>
     </div>
   );
 }
